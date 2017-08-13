09c5661c1a5b64878a6a2755fcef9f14a870cce4
Серж <serj2107@gmail.com>
2017-08-13 18:27:31 +0300

    гистограмма

---
+++ js/stat.js
@@ -0,0 +1,53 @@
+'use strict';
+
+window.renderStatistics = function (ctx, names, times) {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.strokeRect(110, 20, 420, 270);
+  ctx.fillRect(110, 20, 420, 270);
+
+  ctx.fillStyle = '#ffffff';
+  ctx.strokeRect(100, 10, 420, 270);
+  ctx.fillRect(100, 10, 420, 270);
+
+  ctx.fillStyle = '#000000';
+  ctx.font = '16px PT Mono';
+
+  ctx.fillText('Ура вы победили!', 120, 40);
+
+  var max = -1;
+  var maxIndex = -1;
+
+  for (var i = 0; i < times.length; i++) {
+    if (times[i] > max) {
+      max = times[i];
+      maxIndex = i;
+    }
+  }
+
+  ctx.fillText('Худшее время: ' + max, 120, 60);
+
+  var histogramHeight = 150;
+  var step = histogramHeight / max;
+
+  ctx.fillText('Худшее время: ' + max.toFixed(2) + 'мс у игрока ' + names[maxIndex], 120, 60);
+
+  ctx.textBaseline = 'top';
+
+  var initialX = 180;
+  var initialY = 250;
+  var barWidth = 40;
+
+  for (var j = 0; j < times.length; j++) {
+    if (names[j] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'blue';
+      ctx.fillStyle = 'filter: saturate(0)';
+    }
+    ctx.fillRect(initialX, initialY, -barWidth, -times[j] * step);
+    ctx.fillStyle = 'black';
+    ctx.fillText(Math.floor(times[j]), initialX - barWidth, initialY - times[j] * step - 20);
+    ctx.fillText(names[j], initialX - barWidth, 255);
+    initialX += 90;
+  }
+};
