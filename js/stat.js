3ad0ca87c69ccf0e21075f132ca5d422c38b79a2
serj <serj2107@gmail.com>
2017-08-16 10:13:32 +0300

    разбил renderStatistics на несколько функций

--- js/stat.js
+++ js/stat.js
@@ -1,54 +1,58 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.strokeRect(110, 20, 420, 270);
-  ctx.fillRect(110, 20, 420, 270);
 
-  ctx.fillStyle = '#ffffff';
-  ctx.strokeRect(100, 10, 420, 270);
-  ctx.fillRect(100, 10, 420, 270);
+  renderStatisticsField(ctx, 100, 10, 420, 270);
 
   ctx.fillStyle = '#000000';
   ctx.font = '16px PT Mono';
 
   ctx.fillText('Ура вы победили!', 120, 40);
 
+  ctx.fillText('Список результатов:', 120, 60);
+
+  ctx.textBaseline = 'top';
+
+  var max = maxTime(times);
+  var histogramHeight = 150;
+  var step = histogramHeight / max;
+
+  renderHistogram(ctx, step, 180, 250, 40, names, times);
+};
+
+function maxTime(times) {
   var max = -1;
-  var maxIndex = -1;
 
   for (var i = 0; i < times.length; i++) {
     if (times[i] > max) {
       max = times[i];
-      maxIndex = i;
     }
   }
 
-  max = max.toFixed(2);
+  return max;
+}
 
-  ctx.fillText('Худшее время: ' + max, 120, 60);
-
-  var histogramHeight = 150;
-  var step = histogramHeight / max;
-
-  ctx.fillText('Худшее время: ' + max + 'мс у игрока ' + names[maxIndex], 120, 60);
-
-  ctx.textBaseline = 'top';
-
-  var initialX = 180;
-  var initialY = 250;
-  var barWidth = 40;
-
-  for (var j = 0; j < times.length; j++) {
-    if (names[j] === 'Вы') {
+function renderHistogram(ctx, step, initialX, initialY, barWidth, names, times) {
+  for (var i = 0; i < times.length; i++) {
+    if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-      ctx.fillStyle = 'rgba(0, 0, ' + Math.floor(Math.random() * (255 + 1)) + ', 1)';
+      ctx.fillStyle = 'rgba(0, 0, ' + Math.floor(Math.random() * 256) + ', 1)';
     }
-    ctx.fillRect(initialX, initialY, -barWidth, -times[j] * step);
+    ctx.fillRect(initialX, initialY, -barWidth, -times[i] * step);
     ctx.fillStyle = 'black';
-    ctx.fillText(Math.floor(times[j]), initialX - barWidth, initialY - times[j] * step - 20);
-    ctx.fillText(names[j], initialX - barWidth, 255);
+    ctx.fillText(Math.floor(times[i]), initialX - barWidth, initialY - times[i] * step - 20);
+    ctx.fillText(names[i], initialX - barWidth, 255);
     initialX += 90;
   }
-};
+}
+
+function renderStatisticsField(ctx, x, y, width, height) {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.strokeRect(x + 10, y + 10, width, height);
+  ctx.fillRect(x + 10, y + 10, width, height);
+
+  ctx.fillStyle = '#ffffff';
+  ctx.strokeRect(x, y, width, height);
+  ctx.fillRect(x, y, width, height);
+}
